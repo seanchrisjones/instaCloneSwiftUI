233bff6a39c7ffc7992893db2fc017ac6df0d66a
Sean <s.christopherjones@gmail.com>
2021-12-07 16:23:43 -0500

    add scrollview to FeedViewFile

--- InstagramSwiftUI/InstagramSwiftUI/Views/Feed/FeedView.swift
+++ InstagramSwiftUI/InstagramSwiftUI/Views/Feed/FeedView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Feed")
+        ScrollView {
+            ForEach(0..<100
+            ) { _ in
+                Text("Feed")
+            }
+        }
     }
 }
 
