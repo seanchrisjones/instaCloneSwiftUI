e387ff45f03f8e9d582cddc800a6b0d1aadf829c
Sean <s.christopherjones@gmail.com>
2021-12-08 15:54:36 -0500

    add navigation link from userlist view to profileView

--- InstagramSwiftUI/InstagramSwiftUI/Views/Items/UserListView.swift
+++ InstagramSwiftUI/InstagramSwiftUI/Views/Items/UserListView.swift
@@ -12,8 +12,13 @@ struct UserListView: View {
         ScrollView {
             LazyVStack {
                 ForEach(0..<20) { _ in
-                    UserCell()
-                        .padding(.leading)
+                    NavigationLink(
+                    destination: ProfileView(),
+                    label: {
+                        UserCell()
+                            .padding(.leading)
+                    })
+                        
                 }
             }
         }
