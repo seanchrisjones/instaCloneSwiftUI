010276d84153fc0f0c6e276292cbd86afbbdecd7
Sean <s.christopherjones@gmail.com>
2021-12-08 14:29:14 -0500

    add user cell and user list views

---
+++ InstagramSwiftUI/InstagramSwiftUI/Views/Items/UserListView.swift
@@ -0,0 +1,20 @@
+//
+//  UserListView.swift
+//  InstagramSwiftUI
+//
+//  Created by Sean Jones on 12/8/21.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserListView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserListView()
+    }
+}
