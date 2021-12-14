b8f80f5b4d45fc8537ac38fd69b1e98242b13ecf
Sean <s.christopherjones@gmail.com>
2021-12-14 12:43:41 -0500

    build upload Post UI

--- InstagramSwiftUI/InstagramSwiftUI/Views/Profile/UploadPostView.swift
+++ InstagramSwiftUI/InstagramSwiftUI/Views/Profile/UploadPostView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
     var body: some View {
-        Text("Upload post")
+        VStack{
+            if postImage == nil {
+                Button(action: {} , label: {Image(systemName: "plus")
+                        .resizable()
+                        .renderingMode(.template) // allows to change the foreground color
+                        .scaledToFill()
+                        .frame(width: 180, height:180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                })
+            } else {
+                HStack(alignment: .top) {
+                    Image("Goku")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption...", text: $captionText)
+                }.padding()
+                
+                Button(action: {}, label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width:360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }).padding()
+            }
+            
+            Spacer()
+        }
     }
 }
 
