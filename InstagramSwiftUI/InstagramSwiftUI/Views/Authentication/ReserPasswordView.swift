f2a35929f3c30fc49d346a91db6e6c0b58e21f16
Sean <s.christopherjones@gmail.com>
2021-12-15 09:34:48 -0500

    add files for registration, login, and reset password views

---
+++ InstagramSwiftUI/InstagramSwiftUI/Views/Authentication/ReserPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  ReserPasswordView.swift
+//  InstagramSwiftUI
+//
+//  Created by Sean Jones on 12/15/21.
+//
+
+import SwiftUI
+
+struct ReserPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReserPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReserPasswordView()
+    }
+}
